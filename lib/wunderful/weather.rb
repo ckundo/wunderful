d0a654c0e62e27c1d0a6735030b8b6c37274b5bb
Cameron Cundiff <ckundo@gmail.com>
2012-03-18 18:28:04 -0400

    removed current conditions functionality. keeping it simple!

--- lib/wunderful/weather.rb
+++ lib/wunderful/weather.rb
@@ -6,21 +6,18 @@ module Wunderful
     base_uri 'http://api.wunderground.com'
     format :json
 
-    attr_reader :alerts, :conditions, :state, :city, :lat, :lng
+    attr_reader :report, :alerts, :lat, :lng
 
     def initialize(zip, *args)
       @zip = zip
-      @features = %w{ geolookup conditions alerts } + args
+      @features = %w{ geolookup alerts } + args
       @features.flatten!
-      report = self.class.get("/api/#{Wunderful::API_TOKEN}/#{@features.join('/')}/q/#{@zip}.json")
+      @report = self.class.get("/api/#{Wunderful::API_TOKEN}/#{@features.join('/')}/q/#{@zip}.json")
 
-      @state = report['location']['state']
-      @city = report['location']['city']
       @lat = report['location']['lat'].to_f
       @lng = report['location']['lon'].to_f
 
       @alerts = report['alerts']
-      @conditions = report['current_observation']
     end
   end
 end
--- spec/wunderful_spec.rb
+++ spec/wunderful_spec.rb
@@ -3,24 +3,19 @@ require "api_token"
 
 describe Wunderful::Weather do
   before do
-    location = '40.77,93.98' # accepts a zipcode or lat/lng
+    # accepts a zipcode or lat/lng
+    location = '40.77,93.98' # NYC
     @weather = Wunderful::Weather.new(location)
   end
 
   it "should have an alerts array" do
     @weather.alerts.should be_an_instance_of Array
   end
-
-  #it "should have current observations" do
-    #@weather.conditions.should_not be_nil
-  #end
   
-  #it "should have location details" do
-    #@weather.city.should be_an_instance_of String
-    #@weather.state.should be_an_instance_of String
-    #@weather.lat.should be_an_instance_of Float
-    #@weather.lng.should be_an_instance_of Float
-  #end
+  it "should have location details" do
+    @weather.lat.should be_an_instance_of Float
+    @weather.lng.should be_an_instance_of Float
+  end
 
   context 'an alert' do
     before do
