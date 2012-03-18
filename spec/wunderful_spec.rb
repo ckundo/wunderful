597eab3a988b9a8ac27aa2971f48af21719dca1e
Cameron Cundiff <ckundo@gmail.com>
2012-03-18 18:24:44 -0400

    adding alert specs

--- spec/wunderful_spec.rb
+++ spec/wunderful_spec.rb
@@ -11,14 +11,72 @@ describe Wunderful::Weather do
     @weather.alerts.should be_an_instance_of Array
   end
 
-  it "should have current observations" do
-    @weather.conditions.should_not be_nil
-  end
+  #it "should have current observations" do
+    #@weather.conditions.should_not be_nil
+  #end
   
-  it "should have location details" do
-    @weather.city.should be_an_instance_of String
-    @weather.state.should be_an_instance_of String
-    @weather.lat.should be_an_instance_of Float
-    @weather.lng.should be_an_instance_of Float
+  #it "should have location details" do
+    #@weather.city.should be_an_instance_of String
+    #@weather.state.should be_an_instance_of String
+    #@weather.lat.should be_an_instance_of Float
+    #@weather.lng.should be_an_instance_of Float
+  #end
+
+  context 'an alert' do
+    before do
+      @alert = @weather.alerts.first
+    end
+
+    it 'should have a location' do
+      @alert.latitude.should be_an_instance_of Float
+      @alert.longitude.should be_an_instance_of Float
+      @alert.area.should be_an_instance_of String
+    end
+
+    it 'should have an event category' do
+      # Geo, Met, Safety, Security, Rescue, Fire, Health, Env, Transport, Infra, CBRNE, Other
+    end
+
+    # Tornado Watch, Hurricane Watch, etc
+    it 'should have an event description' do
+      @alert.description.should be_an_instance_of String
+      @alert.description.should_not be_empty
+    end
+
+    it 'should have a summary' do
+      @alert.summary.should be_an_instance_of String
+      @alert.summary.should_not be_empty
+    end
+
+    it 'should have an onset time' do
+      @alert.time_effective.should be_an_instance_of DateTime
+    end
+
+    it 'should have instructions' do
+      @alert.instructions.should be_an_instance_of String
+      @alert.instructions.should_not be_empty
+    end
+
+    it 'should have an expiration time' do
+      @alert.time_expires.should be_an_instance_of DateTime
+    end
+
+    # indicates time to take action
+    # now, next hour, soon, no action required, unknown
+    it 'should have a level of urgency' do
+      # immediate, expected future, past, unknown
+    end
+
+    # indicates threat to life and property
+    # extraordinary threat, significant threat, possible threat, minimal threat, unknown
+    it 'should have a level of severity' do
+      # extreme, severe, moderate, minor, unknown
+    end
+
+    # happening/happened, prob ~ 100%, prob > 50%, prob ~ 0
+    it 'should have a level of certainty' do
+      # observed, expected, likely, unlikely
+    end
+
   end
 end
